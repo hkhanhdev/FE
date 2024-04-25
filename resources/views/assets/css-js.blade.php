9d5b7936043cd4cde2be8e04937ff258256bfbd8
hoang <107757542+hkhanhdev@users.noreply.github.com>
2024-04-25 09:16:04 +0700

    js script updated

--- resources/views/assets/css-js.blade.php
+++ resources/views/assets/css-js.blade.php
@@ -1,2 +1,4 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+{{--js script for carousel :))--}}
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
