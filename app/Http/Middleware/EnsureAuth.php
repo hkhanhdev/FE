63efd9b90b344ff901f4f965be40c20ccae8afee
hoang <107757542+hkhanhdev@users.noreply.github.com>
2024-06-12 09:40:42 +0700

    add custom auth middleware

---
+++ app/Http/Middleware/EnsureAuth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Session::has("access_token")) {
+            return Redirect::route('sign_in');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/',[\App\Http\Controllers\Client\Home::class,"home"])->name('home');
 Route::get('/all',[\App\Http\Controllers\Client\Home::class,"pagination"]);
 
 Route::get('/sign_in',[\App\Http\Controllers\Client\Home::class,"toSignIn"])->name('sign_in');
-Route::post('/sign_in',[\App\Http\Controllers\Auth\AuthenticateUser::class,"authenticate"]);
+Route::post('/sign_in',[\App\Http\Controllers\Auth\AuthenticateUser::class,"authenticate"])->middleware(['authenticated']);
 
 Route::get('/sign_up',[\App\Http\Controllers\Client\Home::class,"toSignUp"])->name('sign_up');
 Route::post('/sign_up',[\App\Http\Controllers\Auth\Register::class,"registeringUser"]);
@@ -17,21 +17,21 @@ Route::get('/product',[\App\Http\Controllers\Client\Home::class,'productDetails'
 
 Route::get('/products',[\App\Http\Controllers\Client\Home::class,"productsByCate"]);
 
-Route::get('/cart',[\App\Http\Controllers\Client\Cart::class,"render_cart"])->name('cart');
-Route::put('/cart',[\App\Http\Controllers\Client\Cart::class,"update_cart"]);
-Route::delete('/cart',[\App\Http\Controllers\Client\Cart::class,"remove_item"]);
-Route::post('/cart',[\App\Http\Controllers\Client\Cart::class,"to_check_out"]);
+Route::get('/cart',[\App\Http\Controllers\Client\Cart::class,"render_cart"])->name('cart')->middleware(['authenticated']);
+Route::put('/cart',[\App\Http\Controllers\Client\Cart::class,"update_cart"])->middleware(['authenticated']);
+Route::delete('/cart',[\App\Http\Controllers\Client\Cart::class,"remove_item"])->middleware(['authenticated']);
+Route::post('/cart',[\App\Http\Controllers\Client\Cart::class,"to_check_out"])->middleware(['authenticated']);
 Route::get('/check_out',function (){
     return view('pages.check_out');
-})->name('checkout');
-Route::post('/check_out',[\App\Http\Controllers\Client\Cart::class,"cf_check_out"]);
+})->name('checkout')->middleware(['authenticated']);
+Route::post('/check_out',[\App\Http\Controllers\Client\Cart::class,"cf_check_out"])->middleware(['authenticated']);
 
-Route::get('/order_history',[\App\Http\Controllers\Client\Home::class,'order_history'])->name('order_history');
+Route::get('/order_history',[\App\Http\Controllers\Client\Home::class,'order_history'])->name('order_history')->middleware(['authenticated']);
 
-Route::get('/profile',[\App\Http\Controllers\Client\Home::class,'profile'])->name('profile');
-Route::post('/profile',[\App\Http\Controllers\Client\Home::class,'updateInfo']);
+Route::get('/profile',[\App\Http\Controllers\Client\Home::class,'profile'])->name('profile')->middleware(['authenticated']);
+Route::post('/profile',[\App\Http\Controllers\Client\Home::class,'updateInfo'])->middleware(['authenticated']);
 
-Route::get('/logout',[\App\Http\Controllers\Client\Home::class,"logout"]);
+Route::get('/logout',[\App\Http\Controllers\Client\Home::class,"logout"])->middleware(['authenticated']);
 
 Route::fallback(function () {
     return view("pages.fallback");
